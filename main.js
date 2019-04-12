6804deb6fd04602ffb5871993fad0f1f00f0eae8
Dan Ross <daniellross@gmail.com>
2019-04-12 16:42:08 -0400

    added address readout to html

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ document.getElementById('getAddress').addEventListener('click', runGeocode)
 
 const geocodeURL = 'http://www.mapquestapi.com/geocoding/v1/address?key=yH5iS2qacftnKf9BfA1LlAPS8JwsAn8S&location=1600+Pennsylvania+Ave+NW,Washington,DC,20500'
 
-const issURL = ''
+const issURL = 'http://api.open-notify.org/iss-pass.json?lat=LAT&lon=LON'
 
 function runGeocode() {
     console.log('You submitted an address... here is the latitude and longitude')
@@ -16,5 +16,9 @@ function runGeocode() {
             console.log(data);
             let latitude = data["results"][0]["locations"][0]["latLng"]["lat"]
             document.getElementById('latitude').innerHTML = "Your latitude:" + " " + latitude
+            let longitude = data["results"][0]["locations"][0]["latLng"]["lng"]
+            document.getElementById('longitude').innerHTML = "Your longitude:" + " " + longitude
+            let userAddress = data["results"][0]["locations"][0]["street"] + ", " + data["results"][0]["locations"][0]["adminArea5"] + ", " + data["results"][0]["locations"][0]["adminArea3"]
+            document.getElementById('userAddress').innerHTML = "Your Address:" + " " + userAddress
         })
 }
