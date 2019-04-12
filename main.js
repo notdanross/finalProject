f0aa78a954ce13c647715f69d1090678eda72ea1
Dan Ross <daniellross@gmail.com>
2019-04-12 16:30:34 -0400

    basic function for retrieving latlong works on button click

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 document.getElementById('getAddress').addEventListener('click', runGeocode)
 
-const apiURL = 'http://www.mapquestapi.com/geocoding/v1/address?key=yH5iS2qacftnKf9BfA1LlAPS8JwsAn8S&location=1600+Pennsylvania+Ave+NW,Washington,DC,20500'
+const geocodeURL = 'http://www.mapquestapi.com/geocoding/v1/address?key=yH5iS2qacftnKf9BfA1LlAPS8JwsAn8S&location=1600+Pennsylvania+Ave+NW,Washington,DC,20500'
+
+const issURL = ''
 
 function runGeocode() {
-    console.log('You submitted an address... here is the latude and longitude')
+    console.log('You submitted an address... here is the latitude and longitude')
 
-    fetch()
+    fetch(geocodeURL)
+        .then(function (response) {
+            // console.log(response.json());
+            return (response.json());
+        })
+        .then(function(data){
+            console.log(data);
+            let latitude = data["results"][0]["locations"][0]["latLng"]["lat"]
+            document.getElementById('latitude').innerHTML = "Your latitude:" + " " + latitude
+        })
 }
