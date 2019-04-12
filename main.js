ede9dc0b92e060b84c732a55bd84864bded0b6fe
Dan Ross <daniellross@gmail.com>
2019-04-12 11:13:14 -0400

    started making function to geocode address

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
+document.getElementById('getAddress').addEventListener('click', runGeocode)
 
-const
+const apiURL = 'http://www.mapquestapi.com/geocoding/v1/address?key=yH5iS2qacftnKf9BfA1LlAPS8JwsAn8S&location=1600+Pennsylvania+Ave+NW,Washington,DC,20500'
+
+function runGeocode() {
+    console.log('You submitted an address... here is the latude and longitude')
+
+    fetch()
+}
