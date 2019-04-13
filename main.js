5bb7aa837c4d31718437516bf7ebcd3e2b721f97
Dan Ross <daniellross@gmail.com>
2019-04-13 14:50:22 -0400

    succesfully pulling user submitted info into function

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-document.getElementById('getAddress').addEventListener('click', urlUpdate)
+const userCity  = document.getElementById('city')
+const userState = document.getElementById('state');
 
-document.getElementById('getAddress').addEventListener('click', runGeocode)
 
-const geocodeURL = 'http://www.mapquestapi.com/geocoding/v1/address?key=yH5iS2qacftnKf9BfA1LlAPS8JwsAn8S&location=1600+Pennsylvania+Ave+NW,Washington,DC,20500'
+// document.getElementById('getAddress').addEventListener('click', urlUpdate);
 
-const issURL = 'http://api.open-notify.org/iss-pass.json?lat=LAT&lon=LON'
+document.getElementById('getAddress').addEventListener('click', runGeocode);
+
+// const geocodeURL = 'http://www.mapquestapi.com/geocoding/v1/address?key=yH5iS2qacftnKf9BfA1LlAPS8JwsAn8S&location=1600+Pennsylvania+Ave+NW,Washington,DC,20500';
+const geocodeURL = 'http://www.mapquestapi.com/geocoding/v1/address?key=yH5iS2qacftnKf9BfA1LlAPS8JwsAn8S&location=';
+
+const issURL = 'http://api.open-notify.org/iss-pass.json?lat=LAT&lon=LON';
 
 function urlUpdate() {
     // commented out section is mirrored from eloquent javascript
@@ -13,14 +18,16 @@ function urlUpdate() {
     //     console.log("Saving value", form.elements.value.value);
     //     event.preventDefault();
     // });
-    console.log(document.getElementById('state').getAttribute(userState))
+    console.log(userState)
     // console.log('the button works')
 };
 
 function runGeocode() {
-    console.log('You submitted an address... here is the latitude and longitude')
+    event.preventDefault();
+    console.log('You submitted an address... here is the latitude and longitude');
+    console.log(userState.value);
 
-    fetch(geocodeURL)
+    fetch(geocodeURL + userCity.value + userState.value)
         .then(function (response) {
             // console.log(response.json());
             return (response.json());
@@ -32,8 +39,6 @@ function runGeocode() {
             let longitude = data["results"][0]["locations"][0]["latLng"]["lng"];
             document.getElementById('longitude').innerHTML = "Your longitude:" + " " + longitude;
             let userAddress = data["results"][0]["locations"][0]["street"] + ", " + data["results"][0]["locations"][0]["adminArea5"] + ", " + data["results"][0]["locations"][0]["adminArea3"];
-            document.getElementById('userAddress').innerHTML = "Your Address:" + " " + userAddress;
-            
-        })
-    event.preventDefault();
-}
+            document.getElementById('userAddress').innerHTML = "Your Address:" + " " + userAddress;  
+        })  
+};
